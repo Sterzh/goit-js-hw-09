acfb34adfebd42b24ea27e064f895af87adcf105
Serhii Tomashevskyi <tom5423000@gmail.com>
2023-02-27 01:51:50 +0200

    save 27.02.23 (1:51)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
 
+buttonStart.addEventListener('click', startSwitcher);
+buttonStart.addEventListener('click', stopSwitcher);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function startSwitcher() {
+  changeColor();
+}
+
+function stopSwitcher() {
+  window.clearInterval(timerId);
+}
