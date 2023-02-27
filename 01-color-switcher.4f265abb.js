de188ea575dfa6a0afaa54065769eb47894bae83
Sterzh <112435701+Sterzh@users.noreply.github.com>
2023-02-27 15:49:37 +0000

    Deploying to gh-pages from @ Sterzh/goit-js-hw-09@08e292f701fc27205cade42fdb5b459c9c962c52 🚀

--- 01-color-switcher.4f265abb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");let e=null;t.addEventListener("click",(function(){e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(){window.clearInterval(e)}));
-//# sourceMappingURL=01-color-switcher.4f265abb.js.map
--- 01-color-switcher.8344b677.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),n=(document.querySelector("button[data-stop]"),null);t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){window.clearInterval(n)}))}();
-//# sourceMappingURL=01-color-switcher.8344b677.js.map
