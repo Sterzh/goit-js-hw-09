95379e642c9ab575d32744b92aa04246eef245d8
Sterzh <112435701+Sterzh@users.noreply.github.com>
2023-03-03 04:57:43 +0000

    Deploying to gh-pages from @ Sterzh/goit-js-hw-09@3922bf2b631f05755166c42b24b293814874bf4b 🚀

---
+++ 01-color-switcher.14f16ba9.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function d(){n=clearInterval(n),t.disabled=!1,e.disabled=!0}t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.addEventListener("click",d),t.disabled=!0,e.disabled=!1})),e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.14f16ba9.js.map
